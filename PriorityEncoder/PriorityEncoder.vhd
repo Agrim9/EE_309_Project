4b27b3eb21293f5acac22c74ee9f70b19bc4c047
Harshvardhan1 <hrtibrewal@gmail.com>
2016-10-20 19:20:39 +0530

    PE update

--- PriorityEncoder/PriorityEncoder.vhd
+++ PriorityEncoder/PriorityEncoder.vhd
@@ -34,7 +34,7 @@ function to_int(x: std_logic_vector) return integer is
   end to_int;
 
 begin
-err_flag <= not ( x(7) or x(6) or x(5) or x(4) or x(3) or x(2) or x(1) or x(0) ) ;
+--err_flag <= not ( x(7) or x(6) or x(5) or x(4) or x(3) or x(2) or x(1) or x(0) ) ;
 s1(0) <= ( x(1) and not x(0) ) or
 	( x(3) and not x(2) and not x(1) and not x(0) ) or
 	( x(5) and not x(4) and not x(3) and not x(2) and
@@ -62,10 +62,26 @@ loc<=location;
 
 process(location)
 variable dat: std_logic_vector (15 downto 0);
+variable gengar: std_logic:='0';
+variable count: integer:=0;
 begin
 dat:=x;
+---
+for I in 0 to 7 loop
+	if(dat(i)='1') then
+		count:=count+1;
+	end if;
+end loop;
+---
+if (count=1 or count =0) then
+	gengar:='1';
+else 
+	gengar:='0';
+end if;
+count:=0;
 dat(location):='0';
 d<=dat;
+err_flag<=gengar;
 end process;
 
 end comb ;
