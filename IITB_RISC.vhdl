2ba94e1a672ed275772ffe7f1fed4b326d3b8756
Agrim Gupta <agrim9gupta@gmail.com>
2016-10-24 10:51:02 +0200

    Grand Entity for Testbench Purposes

--- IITB_RISC.vhdl
+++ IITB_RISC.vhdl
@@ -7,7 +7,10 @@ use work.IITB_RISC_Components.all;
 entity IITB_RISC is port(
 	start_mc: in std_logic;
 	reset: in std_logic;
-	clk: in std_logic
+	clk: in std_logic;
+	instr_data_in: in std_logic_vector(15 downto 0);
+	addr_wr: in std_logic_vector(15 downto 0);
+	mem_bit: in std_logic
 );
 end entity IITB_RISC;
 
@@ -17,7 +20,9 @@ architecture Struct of IITB_RISC is
    signal IR_val: std_logic_vector(15 downto 0);
    signal Carry,Zero,DoneCP: std_logic;
    signal P: std_logic_vector(4 downto 0);
-   signal Mem_Dout,Mem_Din,Mem_Ain:std_logic_vector(15 downto 0);
+   signal Mem_Dout,Mem_Din,Mem_Ain, bitwise_and_i,bitwise_and_d, RAM_d_in,RAM_a_in:std_logic_vector(15 downto 0);
+   signal mem_write:std_logic:='0';
+
 begin
 
     CP: ControlPath 
@@ -38,12 +43,17 @@ begin
 	  Mem_Din=>Mem_Din,
 	  Mem_Ain=>Mem_Ain,     -- data and address input to memory
 	  CLK=>clk);
-
+	
+	bitwise_and_i <= (others => mem_bit);
+	bitwise_and_d <= (others => (not mem_bit));
+	mem_write <= T_arr(26) or mem_bit;
+	RAM_d_in <= (instr_data_in and bitwise_and_i) or (Mem_Din and bitwise_and_d); 
+	RAM_a_in <= (addr_wr and bitwise_and_i) or (Mem_Ain and bitwise_and_d);
     RAM_Port: RAM port map (
 	clock  => clk,
-    writeEN=>T_arr(26),
-    address => Mem_Ain,
-    datain  => Mem_Din,
+    writeEN=>mem_write ,
+    address => RAM_a_in,
+    datain  => RAM_d_in,
     dataout =>Mem_Dout
 	);
 
--- IITB_RISC_Components.vhd
+++ IITB_RISC_Components.vhd
@@ -4,6 +4,18 @@ USE IEEE.STD_LOGIC_1164.ALL;
 
 package IITB_RISC_Components is
 
+--Grand Entity
+component IITB_RISC is port(
+	start_mc: in std_logic;
+	reset: in std_logic;
+	clk: in std_logic;
+	instr_data_in: in std_logic_vector(15 downto 0);
+	addr_wr: in std_logic_vector(15 downto 0);
+	mem_bit: in std_logic
+);
+end component;
+
+
 component ControlPath is
 	port (
 		T_arr: out std_logic_vector(0 to 31);
